106f5e5edb13e841dafed797ba2745ca894dbae9
igorakimy <igorakimy@gmail.com>
2023-08-26 14:46:42 +0300

    rendering the root component and strict mode

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,12 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import Welcome from "./pages/Welcome";
 
 const rootElement = document.getElementById("app");
 const root = createRoot(rootElement);
 
+function App() {
+    return <h1>Hello React!</h1>;
+}
+
 root.render(
     <StrictMode>
-        <Welcome />
+        <App />
     </StrictMode>
 );
