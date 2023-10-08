59ee5b7910f9c18bfc132b45820f11b1ccb8d5dd
igorakimy <igorakimy@gmail.com>
2023-10-08 13:44:53 +0300

    feat: add 404 page

--- resources/js/pages/errors/Error404.jsx
+++ resources/js/pages/errors/Error404.jsx
@@ -1,3 +1,17 @@
+import { Button, Result } from "antd";
+import { Link } from "react-router-dom";
+
 export default function Error404() {
-  return <div>404 - Not Found</div>;
+  return (
+    <Result
+      status="404"
+      title="404"
+      subTitle="Sorry, the page you visited does not exist."
+      extra={
+        <Link to="/">
+          <Button type="primary">Back Home</Button>
+        </Link>
+      }
+    />
+  );
 }
